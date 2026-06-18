f2b71e793a5a10a3fb5cb7784d9be2af20a9d7cf
Aluno25076 <aluno25076@ipt.pt>
2026-06-18 10:36:37 +0100

    Criação dos modelos Member, MyUsrem Subscription e Subscribed

---
+++ DW_Projeto_API/DW_Projeto_API/Models/MyUser.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DW_Projeto_API.Models
+{
+    /// <summary>
+    /// Classe para representar os utilizadores da aplicação
+    /// estes dados identificam os utilizadores, independentemente 
+    /// do tipo de utilizador (Membro, Funcionario)
+    /// </summary>
+    public class MyUser
+    {
+        /// <summary>
+        /// Chave Primaria (PK)
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome Completo do utilizador
+        /// </summary>
+        [StringLength(50)]
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatorio!")]
+        [Display(Name = "Nome Completo")]
+        public string Name { get; set; } = "";
+
+        /// <summary>
+        /// Data de nascimento
+        /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatorio!")]
+        [Display(Name = "Data de nascimento")]
+        public DateOnly BirthDate { get; set; }
+
+        /// <summary>
+        /// Número de telephone
+        /// </summary>
+        [Display(Name = "Telemóvel")]
+        [StringLength(19)]
+        [RegularExpression(@"\+?[0-9]{9,18}", ErrorMessage = "O número de telemóvel deve conter apenas dígitos (entre 9 e 18) e pode começar com um sinal de mais.")]
+        public string? CellPhone { get; set; }
+    }
+}
---
+++ DW_Projeto_API/DW_Projeto_API/Models/Subscribed.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DW_Projeto_API.Models
+{
+    /// <summary>
+    /// Classe que do relacionamento entre membros e subscrições
+    /// </summary>
+    //[PrimaryKey(nameof(MemberFK), nameof(SubscriptionFK))] //chave primaria composta
+    public class Subscribed
+    {
+        /// <summary>
+        /// Membro(subscritor)
+        /// </summary>
+        [ForeignKey(nameof(Member))]
+        [Display(Name = "Membro")]
+        public int MemberFK { get; set; }
+        public Member Member { get; set; } = null!;
+
+        /// <summary>
+        /// Subscrição
+        /// </summary>
+        [ForeignKey(nameof(Subscription))]
+        [Display(Name = "Subscription")]
+        public int SubscriptionFK { get; set; }
+        public Subscription Subscription { get; set; } = null!;
+
+        public DateTime ExpirationDate { get; set; }
+    }
+}
